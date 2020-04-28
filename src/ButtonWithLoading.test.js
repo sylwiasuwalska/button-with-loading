525cca688150453275c2a16f49bc637fd0cea213
sylwia.suwalska@gmail.com <sylwia.suwalska@gmail.com>
2020-04-28 18:19:14 +0200

    first tests

--- src/ButtonWithLoading.test.js
+++ src/ButtonWithLoading.test.js
@@ -4,17 +4,6 @@ import { fireEvent, render } from "@testing-library/react";
 import ButtonWithLoading from "./ButtonWithLoading";
 import { action } from "./App"
 
-// zaprojektuj testy, w których upewnisz się, że powyższy komponent działa poprawnie
-
-// komponent ButtonWithLoading działa poprawnie jeśli:
-// - po wyrenderowaniu jest tagiem button
-// - to co trafia do children jest w tagu button
-// - jeśli nic nie ma w propsie children text buttona to "don't push my buttons"
-// - kiedy button zostanie kliknięty wyświetli w buttonie text "loading..." oraz
-// - na kliknięciu wykona się promise przekazaną w propsie action
-// - po wykonaniu się promisy poprawnie, text buttona zmienia się na "success"
-// - po wykonaniu się promisy z błędem, text buttona zmienia się na "error"
-// - ***kiedy button zostanie kliknięty nie będzie można w niego ponownie kliknąć w czasie ładowania
 
 // - po wyrenderowaniu jest tagiem button
 test("renders button", () => {
@@ -76,6 +65,9 @@ test("on click action is called", () => {
   expect(action).toHaveBeenCalled()
 });
 
+
+// - po wykonaniu się promisy poprawnie, text buttona zmienia się na "success"
+// - po wykonaniu się promisy z błędem, text buttona zmienia się na "error"
 
 
 // - ***kiedy button zostanie kliknięty nie będzie można w niego ponownie kliknąć w czasie ładowania
